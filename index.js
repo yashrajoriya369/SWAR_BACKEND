fd362579a9169d7fe0ac6e7d84d496e145eae741
Yash Rajoriya <yashrajoriya369@gmail.com>
2025-09-16 17:13:34 +0530

    final commit

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const PORT = process.env.PORT || 4000;
 const dbConnect = require("./config/dbConnect");
 const app = express();
 const authRouter = require("./routes/authRoute");
+const cookieParser = require("cookie-paser");
+const cors = require("cors");
+const morgan = require("morgan");
 
 dbConnect();
+app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// app.use("/api/user", authRouter);
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
