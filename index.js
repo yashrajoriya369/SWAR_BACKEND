67a4b014108620d4a090ee34712298ea5a0a0131
Yash Rajoriya <yashrajoriya369@gmail.com>
2025-09-17 13:08:33 +0530

    final commit

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const morgan = require("morgan");
 
 dbConnect();
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "https://user-omega-three.vercel.app/",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://user-omega-three.vercel.app/",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
