9d54999d926391a15a2b8b96180a9a90d86d57bc
Yash Rajoriya <yashrajoriya369@gmail.com>
2025-09-24 18:29:05 +0530

    first commit

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -12,7 +12,7 @@ const createQuiz = async (req, res) => {
 
 const getAllQuizzes = async (req, res) => {
   try {
-    const quizzes = await Quiz.find().sort({ createdAt: -1 });
+    const quizzes = await Quiz.find();
     res.status(200).json(quizzes);
   } catch (error) {
     console.error("Error fetching quizzes:", error);
